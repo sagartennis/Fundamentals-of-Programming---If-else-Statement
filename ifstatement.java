0d50a2659fc8c6b5fa7f4b6bf857b2ed6a6aa414
sagartennis <sagarthetennis10k@gmail.com>
2021-05-16 11:02:16 +0530

    Add files via upload

---
+++ ifstatement.java
@@ -0,0 +1,13 @@
+public class ifstatement {
+    public static void main(String[] args) {
+        // Intialize int a and int b to 4 and 5 respectively 
+        int a = 5;
+        int b = 4;
+
+        if (a > b){
+            System.out.println(a + " is greater than " + b );
+        } else{
+            System.out.println(b + " is greater than " + a );
+        }
+    }
+}
